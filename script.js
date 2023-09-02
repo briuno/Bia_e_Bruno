7d0d331b8efc5ad4fb21754c97608a9c909ae19b
Bruno Reche <49537489+briuno@users.noreply.github.com>
2023-09-02 19:46:51 -0300

    Commit inicial
    <3

---
+++ script.js
@@ -0,0 +1,11 @@
+document.querySelectorAll('.scroll-link').forEach(link => {
+    link.addEventListener('click', function(e) {
+        e.preventDefault();
+        const href = this.getAttribute('href');
+        const offsetTop = document.querySelector(href).offsetTop;
+        scroll({
+            top: offsetTop,
+            behavior: 'smooth'
+        });
+    });
+});
